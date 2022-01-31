0dbb8da9f6458000f9c1280b758e7c3bc190a765
redapy <baha.redaaaa@gmail.com>
2022-01-31 13:23:51 +0100

    add the context api to change color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
+import useTheme from '../hooks/useTheme';
 
 //styles
 import './Navbar.css'
 import SearchBar from './SearchBar';
 
 const Navbar = () => {
+    const {color} = useTheme();
     return ( 
-        <div className="navbar">
+        <div className="navbar" style={{background: color}}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
