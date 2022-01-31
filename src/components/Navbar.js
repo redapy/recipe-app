80faf916834c338e68f05cfcec018427c32922fc
redapy <baha.redaaaa@gmail.com>
2022-01-31 13:35:13 +0100

    add reducer for the theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ import './Navbar.css'
 import SearchBar from './SearchBar';
 
 const Navbar = () => {
-    const {color} = useTheme();
+    const {color, changeColor} = useTheme();
     return ( 
-        <div className="navbar" style={{background: color}}>
+        <div className="navbar" onClick={() => changeColor('pink')} style={{background: color}}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
