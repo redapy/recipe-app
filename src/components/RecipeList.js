1ea34c0e127f61d308c72361b06d5d3b9a43d843
redapy <baha.redaaaa@gmail.com>
2022-02-07 15:28:47 +0100

    add the delete option

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom'
 import useTheme from '../hooks/useTheme';
-
+import Trashcan from '../assests/trashcan.svg'
 // styles
 import './RecipeList.css'
+//firestore
+import { db } from "../firebase/config"
+import { doc, deleteDoc } from "firebase/firestore"
 
 const RecipeList = ({ recipes }) => {
 
   const { mode } = useTheme();
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>
+  }
+
+  const handleClick = async (id) => {
+    const recipe = doc(db, 'recipes', id);
+    await deleteDoc(recipe)
   }
   
   return (
@@ -19,6 +27,11 @@ const RecipeList = ({ recipes }) => {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img 
+            className="delete"
+            onClick={() => handleClick(recipe.id)}
+            src={Trashcan} alt="delete icon" 
+          />
         </div>
       ))}
     </div>
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,5 @@
 import { useHistory } from "react-router-dom";
-import { useState, useRef } from "react/cjs/react.development";
+import { useState, useRef } from "react";
 import "./Create.css";
 //firestore
 import { db } from "../../firebase/config"
